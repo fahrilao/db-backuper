dbdfc0acac3aaa262d40cf6aee27bf1c8525607d
fahrilao <fahrilaodesadikin@gmail.com>
2023-07-27 09:45:22 +0700

    Clean the code index file

--- index.ts
+++ index.ts
@@ -8,12 +8,14 @@ import {
   GoogleServiceAccountInterface,
 } from "./third-party/google"
 
+// ** Load ENV Variables
 const {
   DBHOST = "localhost",
   DBPORT = "3306",
   DBUSER = "root",
   DBPASS = "password",
   DBNAME = "anonymous",
+  PREFIX_FILENAME = "",
   GOOGLE_SERVICE_ACCOUNT,
   GOOGLE_FILE_SHARE_ID,
 } = process.env
@@ -22,62 +24,73 @@ if (!GOOGLE_SERVICE_ACCOUNT) {
   console.error("Please provide a GOOGLE_SERVICE_ACCOUNT")
   process.exit(1)
 }
-
 if (!GOOGLE_FILE_SHARE_ID) {
   console.error("Please provide a GOOGLE_FILE_SHARE_ID")
   process.exit(1)
 }
 
+// ** Initialize
 const gsa: GoogleServiceAccountInterface = JSON.parse(
   atob(GOOGLE_SERVICE_ACCOUNT)
 )
-
 const locationDir = path.join(__dirname, "exports")
-
 const currentTime = dayjs().format("DD-MM-YYYY HH.mm.ss")
 const filename = `${locationDir}/${currentTime}`
 
-backupMysql(filename, DBHOST, parseInt(DBPORT), DBUSER, DBPASS, DBNAME)
-  .then(async (filenameSql) => {
-    try {
-      const filenameZip = await bundledToZip(filenameSql)
-      console.log(`Success build to ${filenameZip} file`)
-      fs.unlink(
-        filenameSql,
-        (err) => err && console.log("Can't delete temp data")
-      )
+const run = async () => {
+  // ** Get an exports file
+  let filenameSql: string
 
-      return filenameZip
-    } catch (err) {
-      console.log(`Can't Bundle ${filenameSql} file`, err)
-      fs.unlink(
-        filenameSql,
-        (err) => err && console.log("Can't delete temp data")
-      )
-      throw err
-    }
-  })
-  .then(async (filenameZip) => {
-    const googleDrive = new GoogleDrive(gsa.client_email, gsa.private_key)
+  try {
+    filenameSql = await backupMysql(
+      filename,
+      DBHOST,
+      parseInt(DBPORT),
+      DBUSER,
+      DBPASS,
+      DBNAME
+    )
 
-    try {
-      await googleDrive.createFile(
-        fs.createReadStream(filenameZip),
-        currentTime + ".zip",
-        GOOGLE_FILE_SHARE_ID
-      )
-      fs.unlink(
-        filenameZip,
-        (err) => err && console.log("Can't delete zip data")
-      )
-    } catch (err) {
-      console.log(`Can't Upload ${filenameZip} file`)
-      fs.unlink(
-        filenameZip,
-        (err) => err && console.log("Can't delete zip data")
-      )
-      throw err
-    }
+    console.log(`Success export to ${filenameSql} file`)
+  } catch (err) {
+    console.log(`Can't export that DB`, err)
+    throw err
+  }
 
-    console.log("The program was runned successfully")
-  })
+  // ** Compress to Zip file
+  let filenameZip: string
+  try {
+    filenameZip = await bundledToZip(filenameSql)
+    console.log(`Success build to ${filenameZip} file`)
+    fs.unlink(
+      filenameSql,
+      (err) => err && console.log("Can't delete temp data")
+    )
+  } catch (err) {
+    console.log(`Can't Bundle ${filenameSql} file`, err)
+    fs.unlink(
+      filenameSql,
+      (err) => err && console.log("Can't delete temp data")
+    )
+    throw err
+  }
+
+  // ** Upload to Google Drive
+  const googleDrive = new GoogleDrive(gsa.client_email, gsa.private_key)
+  try {
+    await googleDrive.createFile(
+      fs.createReadStream(filenameZip),
+      `${PREFIX_FILENAME} ${currentTime}.zip`,
+      GOOGLE_FILE_SHARE_ID
+    )
+    fs.unlink(filenameZip, (err) => err && console.log("Can't delete zip data"))
+  } catch (err) {
+    console.log(`Can't Upload ${filenameZip} file`)
+    fs.unlink(filenameZip, (err) => err && console.log("Can't delete zip data"))
+    throw err
+  }
+
+  console.log("The program was runned successfully")
+}
+
+run()
