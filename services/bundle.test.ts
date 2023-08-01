21c1fe70acd5e4ce3b65a6373e7708a5d0fb78d4
fahrilao <fahrilaodesadikin@gmail.com>
2023-08-01 14:32:44 +0700

    Change library adm-zip to archiver

--- services/bundle.test.ts
+++ services/bundle.test.ts
@@ -1,15 +1,53 @@
-import { backupMysql } from "./backup"
-import AdmZip from "adm-zip"
+import { EventEmitter } from "events"
 import { bundledToZip } from "./bundle"
+const archiver = require("archiver")
 
-jest.mock("adm-zip", () =>
+class MockFS extends EventEmitter {
+  createWriteStream() {
+    // Mock the pipe method
+    return this
+  }
+}
+
+class MockArchiver extends EventEmitter {
+  fs: MockFS | null
+  constructor() {
+    super()
+    this.fs = null
+  }
+
+  pipe(mockfs: MockFS) {
+    console.log(mockfs)
+    this.fs = mockfs
+    // Mock the pipe method
+    return this
+  }
+
+  file() {
+    // Mock the file method
+    return this
+  }
+
+  finalize() {
+    // Mock the finalize method
+    if (this.fs) {
+      console.log(this.fs)
+      this.fs.emit("finish")
+    } else this.emit("error")
+    return this
+  }
+}
+
+jest.mock(
+  "fs",
   jest.fn().mockImplementation(() => ({
-    addLocalFile: jest.fn(),
-    writeZip: jest.fn((toFile: string, callback: (err?: Error) => void) =>
-      callback()
-    ),
+    createWriteStream: jest.fn().mockImplementation(() => new MockFS()),
   }))
 )
+
+jest.mock("archiver", () => {
+  return jest.fn().mockImplementation(() => new MockArchiver())
+})
 
 describe("bundledToZip", () => {
   it("should call AdmZip with the correct parameters", async () => {
@@ -17,6 +55,6 @@ describe("bundledToZip", () => {
     const res = await bundledToZip(file)
 
     expect(res).toEqual(file + ".zip")
-    expect(AdmZip).toHaveBeenCalledTimes(1)
+    expect(archiver).toHaveBeenCalledTimes(1)
   })
 })
--- services/bundle.ts
+++ services/bundle.ts
@@ -1,16 +1,34 @@
-import AdmZip from "adm-zip"
+import archiver from "archiver"
+import fs from "fs"
 
-export const bundledToZip = async (file: string): Promise<string> => {
-  const zip = new AdmZip()
-  const filename = file + ".zip"
-  zip.addLocalFile(file)
+const bundledToZip = async (file: string): Promise<string> => {
   return new Promise((resolve, reject) => {
-    zip.writeZip(filename, (err) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(filename)
-      }
+    const filename = file + ".zip"
+
+    const outputZipStream = fs.createWriteStream(filename)
+
+    outputZipStream.on("finish", () => {
+      resolve(filename)
     })
+
+    outputZipStream.on("error", (err) => {
+      reject(err)
+    })
+
+    const archive = archiver("zip", {
+      zlib: { level: 9 }, // Compression level (0-9, 9 being the highest compression)
+    })
+
+    archive.on("error", (err) => {
+      reject(err)
+    })
+
+    archive.pipe(outputZipStream)
+
+    archive.file(file, { name: filename })
+
+    archive.finalize()
   })
 }
+
+export { bundledToZip }
